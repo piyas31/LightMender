a6e652c1fcb1ddba39b36d8236db9dbc372a31cf
Piyas <piyasreza31@gmail.com>
2025-08-08 18:51:58 +0600

    ready for first deploy

--- app.py
+++ app.py
@@ -103,4 +103,5 @@ def download_file(filename):
     return send_from_directory(app.config['PROCESSED_FOLDER'], filename, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
+
